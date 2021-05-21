55342306b993a4f5d295f9b32aca256199301e7b
miron <miron.ivasch@gmail.com>
2021-05-21 18:57:26 +0300

    Wrote Binary Search algorithm

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faSearch} from '@fortawesome/free-solid-svg-icons'
 import './Table.css';
@@ -12,6 +12,8 @@ function Table(props) {
 
     //for search
     const [searchQuery, setSearchQuery] = useState('');
+    //for search Rating
+    const [searchQueryRating, setSearchQueryRating] = useState('');
 
     //for input ul > li
     const [isOpen, setIsOpen] = useState(true);
@@ -40,7 +42,7 @@ function Table(props) {
         setSortingOrder(sortType);
     }
 
-
+    //linear search
     const filteredFilms = sortArr.filter(item => {
         const searchItem = (title) => {
             return title.toString().toLowerCase().includes(searchQuery.trim().toLowerCase());
@@ -50,10 +52,67 @@ function Table(props) {
             return true;
         }
 
-       return props.columns.some(col => {
+        return props.columns.some(col => {
             return searchItem(item[col.acessor]);
         })
-    })
+
+    });
+
+
+    //BinarySearch
+    const binarySearch = (arr, target) => {
+        let left = 0;
+        let right = arr.length - 1;
+        let mid;
+
+        if (searchQueryRating === '') {
+            return -1;
+        }
+
+        while (left <= right) {
+            mid = Math.round((right - left) / 2) + left;
+
+            if (+target === arr[mid]) {
+                return mid;
+            } else if (+target < arr[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    const ratingArr = sortArr.map((film) => film.assessment);
+    const indexSearch = binarySearch(ratingArr, searchQueryRating);
+
+    const drawTableBinarySearch = (indexSearch) => {
+        if (indexSearch === -1) {
+            return;
+        }
+
+        let outputArray = [indexSearch];
+        let i = indexSearch;
+
+        while (ratingArr.length > i + 1 && ratingArr[i] === ratingArr[i + 1]) {
+            outputArray.push(i + 1);
+            i++;
+        }
+
+        i = indexSearch;
+        while (i - 1 >= 0 && ratingArr[i] === ratingArr[i - 1]) {
+            outputArray.push(i - 1);
+            i--;
+        }
+
+        return outputArray.map((elem, indexSearch) => {
+            console.log(sortArr[outputArray[indexSearch]], 'BinarySearch');
+            return sortArr[outputArray[indexSearch]];
+        })
+    }
+    drawTableBinarySearch(indexSearch);
+
 
     //for input ul > li
     const itemClickHendler = (e) => {
@@ -95,7 +154,8 @@ function Table(props) {
                 <div className="searchInputs__item"><input className="searchInputs__search"
                                                            type="number"
                                                            name="q"
-                                                           placeholder="Search by Rating"/>
+                                                           placeholder="Search by Rating"
+                                                           onChange={(event) => setSearchQueryRating(event.target.value)}/>
                 </div>
             </div>
 
