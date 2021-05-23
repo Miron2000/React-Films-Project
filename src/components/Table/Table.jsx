d8e09175f19f4d927a5cebd7d7449955ab2f773d
miron <miron.ivasch@gmail.com>
2021-05-23 16:10:10 +0300

    Refactoring code

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -3,33 +3,39 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faSearch} from '@fortawesome/free-solid-svg-icons'
 import './Table.css';
 import sortTable from "../../TableOperations/sortTable";
+import {binarySearch, drawTableBinarySearch} from "../../TableOperations/binarySearch";
 
 function Table(props) {
 
     const [activeColumnIndex, setActiveColumnIndex] = useState(-1)
-    const [sortingOrder, setSortingOrder] = useState('ASC');//'DESC' - по спаданию
+    const [sortingOrder, setSortingOrder] = useState('');//'DESC' - по спаданию
     const [activeColumnAcessor, setActiveColumnAcessor] = useState(-1);
+
+    const sortArr = sortTable(props.data, activeColumnAcessor, props.columns, sortingOrder);
 
     //for search
     const [searchQuery, setSearchQuery] = useState('');
     //for search Rating
     const [searchQueryRating, setSearchQueryRating] = useState('');
+    const [resultSearch, setResultSearch] = useState(props.data);
 
     //for input ul > li
     const [isOpen, setIsOpen] = useState(true);
 
-    const sortArr = sortTable(props.data, activeColumnAcessor, props.columns, sortingOrder);
 
     const createTableColumns = (item) => {
         return props.columns.map((i) => {
             return <td key={i.acessor}>{item[i.acessor]}</td>;
         })
     }
-    const createTableRows = () => {
-        return filteredFilms.map((item) => {
-            return <tr key={item.number}>{createTableColumns(item)}</tr>
-        })
-    }
+
+    useEffect(() => {
+        setResultSearch(props.data);
+    }, [props.data]);
+
+    useEffect(() => {
+        setResultSearch(sortArr);
+    }, [sortingOrder])
 
 
     const handleClickTitle = (item) => {
@@ -58,65 +64,23 @@ function Table(props) {
 
     });
 
+    useEffect(() => {
+        setResultSearch(filteredFilms);
+    }, [searchQuery]);
 
-    //BinarySearch
-    const binarySearch = (arr, target) => {
-        let left = 0;
-        let right = arr.length - 1;
-        let mid;
-
-        if (searchQueryRating === '') {
-            return -1;
-        }
-
-        while (left <= right) {
-            mid = Math.round((right - left) / 2) + left;
-
-            if (+target === arr[mid]) {
-                return mid;
-            } else if (+target < arr[mid]) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        return -1;
-    }
 
     const ratingArr = sortArr.map((film) => film.assessment);
     const indexSearch = binarySearch(ratingArr, searchQueryRating);
+    const arrBinarySearch = drawTableBinarySearch(indexSearch, ratingArr, sortArr);
 
-    const drawTableBinarySearch = (indexSearch) => {
-        if (indexSearch === -1) {
-            return;
-        }
 
-        let outputArray = [indexSearch];
-        let i = indexSearch;
-
-        while (ratingArr.length > i + 1 && ratingArr[i] === ratingArr[i + 1]) {
-            outputArray.push(i + 1);
-            i++;
-        }
-
-        i = indexSearch;
-        while (i - 1 >= 0 && ratingArr[i] === ratingArr[i - 1]) {
-            outputArray.push(i - 1);
-            i--;
-        }
-
-        return outputArray.map((elem, indexSearch) => {
-            console.log(sortArr[outputArray[indexSearch]], 'BinarySearch');
-            return sortArr[outputArray[indexSearch]];
-        })
-    }
-    drawTableBinarySearch(indexSearch);
-
+    useEffect(() => {
+        setResultSearch(arrBinarySearch || props.data)
+    }, [searchQueryRating])
 
     //for input ul > li
     const itemClickHendler = (e) => {
-        setSearchQuery(e.target.textContent);
+        setSearchQueryRating(e.target.textContent);
         setIsOpen(false);
 
     }
@@ -135,27 +99,29 @@ function Table(props) {
                            name="q"
                            placeholder="Search..."
                            value={searchQuery}
-                           onChange={(event) => setSearchQuery(event.target.value)}
-                           onClick={inputClickHandler}/>
+                           onChange={(event) => setSearchQuery(event.target.value)}/>
                     <span className='icon__search'><FontAwesomeIcon icon={faSearch}/></span>
 
-                    <ul className='autocomplete'>
-                        {
-                            searchQuery && isOpen ? filteredFilms.map((item) => {
-                                return (
-                                    <li className='searchInputs__item'
-                                        key={item.number} onClick={itemClickHendler}>{item.films}</li>
-                                )
-                            }) : null
-                        }
-                    </ul>
+
 
                 </div>
                 <div className="searchInputs__item"><input className="searchInputs__search"
                                                            type="number"
                                                            name="q"
                                                            placeholder="Search by Rating"
-                                                           onChange={(event) => setSearchQueryRating(event.target.value)}/>
+                                                           onChange={(event) => setSearchQueryRating(event.target.value)}
+                                                           onClick={inputClickHandler}/>
+
+                    <ul className='autocomplete'>
+                        {
+                            searchQueryRating && isOpen ? sortArr.map((item) => {
+                                return (
+                                    <li className='searchInputs__item'
+                                        key={item.number} onClick={itemClickHendler}>{item.assessment}</li>
+                                )
+                            }) : null
+                        }
+                    </ul>
                 </div>
             </div>
 
@@ -168,7 +134,7 @@ function Table(props) {
                     </tr>
                     </thead>
                     <tbody>
-                    {createTableRows()}
+                    {resultSearch?.map((item) => <tr key={item.number}>{createTableColumns(item)}</tr>)}
                     </tbody>
                 </table>
             </div>
