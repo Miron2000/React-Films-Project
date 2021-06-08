ba95e547f7db776afdca7154a1a971831e63b9a3
miron <miron.ivasch@gmail.com>
2021-06-09 01:09:49 +0300

    Started write CRUD operations

--- server/controllers/filmController.js
+++ server/controllers/filmController.js
@@ -16,19 +16,27 @@ class filmController {
         // return res.json(films);
 
         const films = await Film.findAll();
-        console.log(films)
-        res.send(films);
+        try {
+            res.send(films);
+        } catch (err) {
+            next(ApiError.badRequest(err.message))
+        }
+
     }
 
-    async getFilmById(req, res) {
-        const id = req.params.id;
-
+    async getFilmById(req, res, next) {
+        const film = await Film.findByPk(req.params.id);
+        try {
+            res.send(film);
+        } catch (err) {
+            next(ApiError.badRequest(err.message));
+        }
     }
 
     async addFilm(req, res, next) {
-        try{
-            const {name, genre, releaseDate, country, assessment, imdbFilm} = req.body
-            const type = await Film.create({name, genre, releaseDate, country, assessment, imdbFilm});
+        try {
+            const {id, name, releaseDate, assessment, imdbFilm} = req.body
+            const type = await Film.create({id, name, releaseDate, assessment, imdbFilm});
             return res.json({type});
         } catch (err) {
             next(ApiError.badRequest(err.message));
@@ -37,7 +45,33 @@ class filmController {
     }
 
     async updateFilm(req, res) {
+        if (!req.body) {
+            return res
+                .status(400)
+                .send({message: 'Data to update can not be empty'})
+        }
+        const id = req.params.id;
+        const update = await Film.update(req.body, {
+            where: {id: id}
+        }).then(num => {
+            console.log(num, 'NUM')
+            // if (num === 1) {
+            //     res.send({
+            //         message: "Film was updated successfully."
+            //     });
+            // } else {
+            //     res.send({
+            //         message: `Cannot update Film with id=${id}. Maybe Film was not found or req.body is empty!`
+            //     });
+            // }
+        })
+            // .catch(err => {
+            //     res.status(500).send({
+            //         message: `Error updating Film with id= ${id}`
+            //     });
+            // })
 
+        res.send(update);
     }
 
     async deleteFilm(req, res) {
--- server/controllers/operationsDBController.js
+++ server/controllers/operationsDBController.js
@@ -21,72 +21,30 @@ class operationsDBController {
             console.log(err);
             return null;
         }));
-        // let setFilmGenreRequests = filmGenreArr.map((entity) => filmGenre.create(entity));
-        // let setFilmCountryRequests = filmCountryArr.map((entity) => filmCountry.create(entity));
+
+        // let setFilmGenreRequests = filmGenreArr.map((entity) => filmGenre.create(entity).catch((err) => {
+        //     console.log(err);
+        //     return null;
+        // }));
+
+        // let setFilmCountryRequests = filmsArr.map((entity) => filmCountry.create(entity).catch((err) => {
+        //     console.log(err);
+        //     return null;
+        // }));
+
 
         try {
             await Promise.all(setCountryRequests).then(res.send('success'));
             await Promise.all(setGenreRequests).then(res.send('success'))
             await Promise.all(setFilmRequests).then(res.send('success'))
 
-            // await Promise.all(setFilmGenreRequests).then(console.log('success')).catch((err) => console.log(err));
-            // await Promise.all(setFilmCountryRequests).then(console.log('success')).catch((err) => console.log(err));
+            // await Promise.all(setFilmGenreRequests).then(console.log('success'))
+            // await Promise.all(setFilmCountryRequests).then(console.log('success'))
 
             res.status(201).json({message: 'Init DB'});
         } catch (err) {
             res.status(500).json({message: err.message});
         }
-
-
-        // INSERT INTO countries ("name", "code") VALUES
-        // ('USA', 'US'),
-        // ('France', 'FR'),
-        // ('Great Britain, USA', 'GBUS'),
-        // ('Ukraine', 'UA');
-
-        // INSERT INTO genres ("name", "code") VALUES
-        // ('Comedy', 'com'),
-        //     ('Drama', 'dr'),
-        //     ('Drama, crime', 'drcr'),
-        //     ('Horror', 'hor'),
-        //     ('Adventures, Fiction', 'adfi'),
-        //     ('Fiction, Thriller', 'fith');
-
-        // INSERT INTO films ("name", "releaseDate", "assessment", "imdbFilm") VALUES
-        // ('Babysitting', '16.04.2014', 8.7, 'No'),
-        //     ('The Shawshank Redemption', '10.09.1994', 9.5, 'Yes'),
-        //     ('Papillon', '09.09.2017', 8.9, 'Yes'),
-        //     ('The Hangover', '30.05.2009', 9.0, 'Yes'),
-        //     ('Venom', '01.10.2018', 7.8, 'No'),
-        //     ('The Silence of the Lambs', '30.01.1991', 7.2, 'Yes'),
-        //     ('Dumb and Dumber', '16.12.1994', 5.5, 'No'),
-        //     ('WALL-E', '23.06.2008', 8.0, 'Yes'),
-        //     ('1917 (Film)', '04.12.2019', 6.8, 'Yes'),
-        //     ('Transformers', '12.06.2007', 8.0, 'No');
-
-        // INSERT INTO film_countries ("filmId", "countryId") VALUES
-        // (13, 25),
-        //     (14, 24),
-        //     (15, 24),
-        //     (16, 24),
-        //     (17, 24),
-        //     (18, 24),
-        //     (19, 24),
-        //     (20, 24),
-        //     (21, 26),
-        //     (22, 24);
-
-        // INSERT INTO film_genres ("filmId", "genreId") VALUES
-        // (13, 13),
-        //     (14, 14),
-        //     (15, 15),
-        //     (16, 13),
-        //     (17, 15),
-        //     (18, 16),
-        //     (19, 13),
-        //     (20, 17),
-        //     (21, 14),
-        //     (22, 18);
     }
 
     async clearDB(req, res) {
